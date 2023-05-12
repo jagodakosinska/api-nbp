845582a76a96142c87315bbb297bc5bc3fd51e72
jagodakosinska <jagoda.kosinska87@gmail.com>
2023-05-12 09:10:24 +0200

    feat(Repository): CurrencyRepository - add new method for updating and inserting data to the db

---
+++ src/Repository/CurrencyRepository.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Currency;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Currency>
+ *
+ * @method Currency|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Currency|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Currency[]    findAll()
+ * @method Currency[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class CurrencyRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Currency::class);
+    }
+
+    public function flush(): void
+    {
+        $this->getEntityManager()->flush();
+    }
+    public function save(Currency $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function remove(Currency $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function findByCode(string $code): ?Currency
+    {
+        return $this->createQueryBuilder('c')
+            ->andWhere('c.currencyCode = :val')
+            ->setParameter('val', strtoupper($code))
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+}
