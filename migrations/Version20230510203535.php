f2451c709017ee8bccc2c5c2054447dd001017f5
jagodakosinska <jagoda.kosinska87@gmail.com>
2023-05-12 09:04:37 +0200

    feat(Migration): add & push new migration, create db

---
+++ migrations/Version20230510203535.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230510203535 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE currency (id INT NOT NULL, name VARCHAR(255) NOT NULL, currency_code VARCHAR(255) NOT NULL, update_date TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, effective_date DATE NOT NULL, exchange_rate DOUBLE PRECISION NOT NULL, PRIMARY KEY(id))');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE currency');
+    }
+}
