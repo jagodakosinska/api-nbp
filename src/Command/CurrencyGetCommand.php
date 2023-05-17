ba583c50c6d4b85d0ade102e119ed841668c7f74
jagodakosinska <jagoda.kosinska87@gmail.com>
2023-05-17 13:15:47 +0200

    refactor(command): CurrencyGetCommand - clean up used classes

--- src/Command/CurrencyGetCommand.php
+++ src/Command/CurrencyGetCommand.php
@@ -5,13 +5,10 @@ namespace App\Command;
 use App\Entity\Currency;
 use App\Repository\CurrencyRepository;
 use App\Service\ApiNbpCurrencyService;
-use Doctrine\ORM\EntityManager;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
